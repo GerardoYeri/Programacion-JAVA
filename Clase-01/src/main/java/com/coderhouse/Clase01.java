92de7244ab82c316b9c1cf70c5f6ec99e5fe82ef
Geri <Geri@192.168.5.100>
2024-11-13 22:22:56 -0300

    update

--- Clase-01/src/main/java/com/coderhouse/Clase01.java
+++ Clase-01/src/main/java/com/coderhouse/Clase01.java
@@ -5,14 +5,21 @@ public class Clase01 {
 	public static void main(String[] args) {
 		System.out.println("Hola coders!!!");
 		//Declaracion variables
-		byte variableA = 100;
+		byte variableA = 120;
+		
 		double variableConDecimales = -1231231243.52d;
+		
 		char miPrimerChar = 'g';
+		
 		String miPrimerString = "hola coder";
+		
 		boolean verdadeero = false;
-		long numeroLargo = 123425234L;
 		
+		long numeroLargo = 123425234;
+	
+		System.out.println(miPrimerString);
 		
+		System.out.println(miPrimerString + ' ' + miPrimerString);
 
 	}
 
