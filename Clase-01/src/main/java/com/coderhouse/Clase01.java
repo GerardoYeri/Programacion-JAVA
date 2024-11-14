47e0c0c0e57736578fa672a02ff20f7652be84a4
Geri <Geri@192.168.5.100>
2024-11-13 21:43:39 -0300

    update

---
+++ Clase-01/src/main/java/com/coderhouse/Clase01.java
@@ -0,0 +1,19 @@
+package com.coderhouse;
+
+public class Clase01 {
+
+	public static void main(String[] args) {
+		System.out.println("Hola coders!!!");
+		//Declaracion variables
+		byte variableA = 100;
+		double variableConDecimales = -1231231243.52d;
+		char miPrimerChar = 'g';
+		String miPrimerString = "hola coder";
+		boolean verdadeero = false;
+		long numeroLargo = 123425234L;
+		
+		
+
+	}
+
+}
